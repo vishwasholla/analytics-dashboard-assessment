7f7fa88e8ffcd028fb2c4ff756d7247a6f8c7956
vishwasHolla <vishwashollah@gmail.com>
2026-01-30 17:12:51 +0530

    deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/analytical-dashboard/'
 })
 
 // To test responsivenes on small screen
